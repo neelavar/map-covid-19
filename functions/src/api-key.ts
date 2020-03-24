a3a62a9d8cc0562e78e0b5762f6ab83763732794
Prasanna Neelavar <prasanna.neelavar@gmail.com>
2020-03-24 08:03:40 +0530

    chore: add API Keys

---
+++ functions/src/api-key.ts
@@ -0,0 +1,3 @@
+// Sign-up and get API Key from RAPID API: Link: https://rapidapi.com/astsiatsko/api/coronavirus-monitor
+// Test the API endpoint and copy API Key from Rapid API
+export const API_KEY = '************** RAPID API KEY HERE *******************';
---
+++ src/config.js
@@ -0,0 +1,14 @@
+// Copy the Firebase Project "config" from the Firebase Console
+export const firebaseConfig = {
+  apiKey: "****** Firebase API Key ***********",
+  authDomain: "your-project-id.firebaseapp.com",
+  databaseURL: "https://your-project-id.firebaseio.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "999999999999",
+  appId: "1:999999999999:web:1234abcd1234abcd1234abcd",
+  measurementId: "X-9WXYZWXYZWXYZ"
+};
+
+// Get the GMaps API Key: Refer to "https://developers.google.com/maps/documentation/javascript/get-api-key"
+export const GMAP_API_KEY = "*************** GMAP API KEY HERE *************";
