098c0ccb6b3ced64d883e4cbe5d00ec37c3ed5ae
Prasanna Neelavar <prasanna.neelavar@gmail.com>
2020-03-30 19:49:59 +0530

    add totals

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import "firebase/analytics";
-import "firebase/database";
+import 'firebase/analytics';
+import 'firebase/database';
 
-import { GoogleMapsOverlay } from "@deck.gl/google-maps";
-import * as firebase from "firebase/app";
+import { GoogleMapsOverlay } from '@deck.gl/google-maps';
+import * as firebase from 'firebase/app';
 
-import { firebaseConfig, GMAP_API_KEY } from "./config";
-import { darkModeMapStyle } from "./dark-mode-map-style";
-import { heatmap, scatterplot } from "./map-util";
+import { firebaseConfig, GMAP_API_KEY } from './config';
+import { darkModeMapStyle } from './dark-mode-map-style';
+import { heatmap, scatterplot } from './map-util';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -29,21 +29,17 @@ const location = {
 const getMappedCountries = (countries_stat, countries) => {
   const sourceData = [];
   countries_stat.forEach(country => {
-    const cf = countries.find(
-      co => co.name.toLowerCase() === country.country_name.toLowerCase()
-    );
+    const cf = countries.find(co => co.name.toLowerCase() === country.country_name.toLowerCase());
     if (cf) {
       const longitude = cf.long;
       const latitude = cf.lat;
       const flag = cf.flag;
-      const cases = Number(country.cases.replace(",", ""));
-      const deaths = Number(country.deaths.replace(",", ""));
-      const active_cases = Number(country.active_cases.replace(",", ""));
-      const new_cases = Number(country.new_cases.replace(",", ""));
-      const total_recovered = Number(country.total_recovered.replace(",", ""));
-      const serious_critical = Number(
-        country.serious_critical.replace(",", "")
-      );
+      const cases = Number(country.cases.replace(',', ''));
+      const deaths = Number(country.deaths.replace(',', ''));
+      const active_cases = Number(country.active_cases.replace(',', ''));
+      const new_cases = Number(country.new_cases.replace(',', ''));
+      const total_recovered = Number(country.total_recovered.replace(',', ''));
+      const serious_critical = Number(country.serious_critical.replace(',', ''));
       sourceData.push({
         ...country,
         cases,
@@ -61,11 +57,29 @@ const getMappedCountries = (countries_stat, countries) => {
   return sourceData;
 };
 
+const setupTotals = sourceData => {
+  const totalCases = document.getElementById('total-cases');
+  const recoveries = document.getElementById('recoveries');
+  const fatalities = document.getElementById('fatalities');
+
+  let tc = 0;
+  let rc = 0;
+  let ft = 0;
+  sourceData.forEach(d => {
+    tc += d.cases;
+    rc += d.total_recovered;
+    ft += d.deaths;
+  });
+  totalCases.innerHTML = `<b>${tc.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</b>`;
+  recoveries.innerHTML = `<b>${rc.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</b>`;
+  fatalities.innerHTML = `<b>${ft.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</b>`;
+};
+
 // Load the CoVID data from firebase RTDB
 const loadCovidData = callback => {
   return db
-    .ref("/covid-19")
-    .once("value")
+    .ref('/covid-19')
+    .once('value')
     .then(snap => {
       const covid = snap.val();
       const countries = covid.countries;
@@ -73,17 +87,16 @@ const loadCovidData = callback => {
       const sourceData = getMappedCountries(countries_stat, countries);
       date = covid.statistic_taken_at.substring(0, 10);
       // document.title = `CoVID-19 | Updated ${date}`;
-      document.getElementById("updated").innerHTML = `Last Update: ${new Date(
-        date
-      ).toLocaleDateString()}`;
+      document.getElementById('updated').innerHTML = `Last Update: ${new Date(date).toLocaleDateString()}`;
       // console.log({ sourceData });
+      setupTotals(sourceData);
       callback(sourceData);
     });
 };
 
 // On page load, get the GMaps Script & set callback
 window.onload = () => {
-  const gMapScript = document.createElement("script");
+  const gMapScript = document.createElement('script');
   gMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GMAP_API_KEY}&callback=initMap`;
   document.head.appendChild(gMapScript);
 };
@@ -91,19 +104,19 @@ window.onload = () => {
 // callback once the gmaps script is loaded
 window.initMap = () => {
   // define the dark mode map
-  const darkModeMap = new google.maps.StyledMapType(darkModeMapStyle, { name: "CoVID-19 Impact" });
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 23.0, lng: 76.0 },
-    zoom: 5,
-    gestureHandling: "greedy",
+  const darkModeMap = new google.maps.StyledMapType(darkModeMapStyle, { name: 'CoVID-19 Impact' });
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 23.0, lng: 0 },
+    zoom: 3,
+    gestureHandling: 'greedy',
     disableDefaultUI: true,
     mapTypeControlOptions: {
-      mapTypeIds: ["roadmap", "dark_mode_map"]
+      mapTypeIds: ['roadmap', 'dark_mode_map']
     }
   });
 
-  map.mapTypes.set("dark_mode_map", darkModeMap);
-  map.setMapTypeId("dark_mode_map");
+  map.mapTypes.set('dark_mode_map', darkModeMap);
+  map.setMapTypeId('dark_mode_map');
 
   // Now load the CoVID data and setup the overlay layers
   loadCovidData(src => {
@@ -118,7 +131,7 @@ window.initMap = () => {
       // For every location buttons, set click event listeners
       Object.keys(location).forEach(key => {
         const btn = document.getElementById(key);
-        google.maps.event.addDomListener(btn, "click", () => {
+        google.maps.event.addDomListener(btn, 'click', () => {
           // first zoom out a bit
           map.setZoom(3);
           const { lat, lng, zoom } = location[key];
