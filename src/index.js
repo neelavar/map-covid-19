4c441c9fc71e963a814faf10a28d554c494dafe4
Prasanna Neelavar <prasanna.neelavar@gmail.com>
2020-04-05 09:16:49 +0530

    fix: add auth, diameter: based on active_cases

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'firebase/analytics';
 import 'firebase/database';
+import 'firebase/auth';
 
 import { GoogleMapsOverlay } from '@deck.gl/google-maps';
 import * as firebase from 'firebase/app';
@@ -11,25 +12,27 @@ import { heatmap, scatterplot } from './map-util';
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+firebase.auth().signInAnonymously();
 
 const db = firebase.database();
 let date = new Date().toISOString().substring(0, 10);
+let uid = null;
 
 // Locations to pan & zoom into on click of a button
-const location = {
+const locationData = {
   as: { lat: 23, lng: 82, zoom: 4 },
   au: { lat: -25, lng: 133, zoom: 5 },
   eu: { lat: 45, lng: 10, zoom: 5 },
   na: { lat: 42, lng: -95, zoom: 4 },
   me: { lat: 30, lng: 50, zoom: 5 },
-  sa: { lat: -22, lng: -80, zoom: 4 }
+  sa: { lat: -22, lng: -80, zoom: 4 },
 };
 
 // Combine CoVID data countries_stat with the lat,lng and flag
 const getMappedCountries = (countries_stat, countries) => {
   const sourceData = [];
-  countries_stat.forEach(country => {
-    const cf = countries.find(co => co.name.toLowerCase() === country.country_name.toLowerCase());
+  countries_stat.forEach((country) => {
+    const cf = countries.find((co) => co.name.toLowerCase() === country.country_name.toLowerCase());
     if (cf) {
       const longitude = cf.long;
       const latitude = cf.lat;
@@ -50,14 +53,14 @@ const getMappedCountries = (countries_stat, countries) => {
         new_cases,
         longitude,
         latitude,
-        flag
+        flag,
       });
     }
   });
   return sourceData;
 };
 
-const setupTotals = sourceData => {
+const setupTotals = (sourceData) => {
   const totalCases = document.getElementById('total-cases');
   const recoveries = document.getElementById('recoveries');
   const fatalities = document.getElementById('fatalities');
@@ -65,22 +68,25 @@ const setupTotals = sourceData => {
   let tc = 0;
   let rc = 0;
   let ft = 0;
-  sourceData.forEach(d => {
+  sourceData.forEach((d) => {
     tc += d.cases;
     rc += d.total_recovered;
     ft += d.deaths;
   });
-  totalCases.innerHTML = `<b>${tc.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</b>`;
-  recoveries.innerHTML = `<b>${rc.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</b>`;
-  fatalities.innerHTML = `<b>${ft.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</b>`;
+  totalCases.innerHTML = `<b>${tc.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</b>`;
+  recoveries.innerHTML = `<b>${rc.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</b>`;
+  fatalities.innerHTML = `<b>${ft.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</b>`;
 };
 
 // Load the CoVID data from firebase RTDB
-const loadCovidData = callback => {
+const loadCovidData = (callback) => {
+  if (!uid) {
+    return callback([]);
+  }
   return db
     .ref('/covid-19')
     .once('value')
-    .then(snap => {
+    .then((snap) => {
       const covid = snap.val();
       const countries = covid.countries;
       const countries_stat = covid.countries_stat;
@@ -96,9 +102,22 @@ const loadCovidData = callback => {
 
 // On page load, get the GMaps Script & set callback
 window.onload = () => {
-  const gMapScript = document.createElement('script');
-  gMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GMAP_API_KEY}&callback=initMap`;
-  document.head.appendChild(gMapScript);
+  const origin = location.hostname;
+  // console.log({ location });
+  if (origin !== 'map-covid-19.web.app') {
+    confirm('Thank you for your interest. \nPlease visit https://map-covid-19.web.app');
+  } else {
+    const gMapScript = document.createElement('script');
+    gMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GMAP_API_KEY}&callback=initMap`;
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        uid = user.uid;
+        document.head.appendChild(gMapScript);
+      } else {
+        uid = null;
+      }
+    });
+  }
 };
 
 // callback once the gmaps script is loaded
@@ -111,30 +130,30 @@ window.initMap = () => {
     gestureHandling: 'greedy',
     disableDefaultUI: true,
     mapTypeControlOptions: {
-      mapTypeIds: ['roadmap', 'dark_mode_map']
-    }
+      mapTypeIds: ['roadmap', 'dark_mode_map'],
+    },
   });
 
   map.mapTypes.set('dark_mode_map', darkModeMap);
   map.setMapTypeId('dark_mode_map');
 
   // Now load the CoVID data and setup the overlay layers
-  loadCovidData(src => {
+  loadCovidData((src) => {
     // console.log({ src });
     if (src) {
       const overlay = new GoogleMapsOverlay({
-        layers: [heatmap(src), scatterplot(src)]
+        layers: [heatmap(src), scatterplot(src)],
       });
 
       overlay.setMap(map);
 
       // For every location buttons, set click event listeners
-      Object.keys(location).forEach(key => {
+      Object.keys(locationData).forEach((key) => {
         const btn = document.getElementById(key);
         google.maps.event.addDomListener(btn, 'click', () => {
           // first zoom out a bit
           map.setZoom(3);
-          const { lat, lng, zoom } = location[key];
+          const { lat, lng, zoom } = locationData[key];
           setTimeout(() => {
             // now pan to the location center
             map.panTo({ lat, lng });
--- src/map-util.js
+++ src/map-util.js
@@ -1,42 +1,19 @@
-import { HeatmapLayer } from "@deck.gl/aggregation-layers";
-import { ScatterplotLayer } from "@deck.gl/layers";
+import { HeatmapLayer } from '@deck.gl/aggregation-layers';
+import { ScatterplotLayer } from '@deck.gl/layers';
 
-const formatNumber = num => {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+const formatNumber = (num) => {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 };
 
 export const showStats = ({ object, x, y }) => {
-  const el = document.getElementById("tooltip");
+  const el = document.getElementById('tooltip');
   if (object) {
-    const {
-      cases,
-      deaths,
-      total_recovered,
-      serious_critical,
-      active_cases,
-      new_cases,
-      country_name,
-      flag
-    } = object;
-    const dN = deaths
-      ? `<tr><td>⚰️ Deaths</td><td>${formatNumber(deaths)}</td></tr>`
-      : "";
-    const sC = serious_critical
-      ? `<tr> <td>🚑 Serious</td><td>${formatNumber(
-          serious_critical
-        )}</td> </tr>`
-      : "";
-    const aC = active_cases
-      ? `<tr><td>🛌 Active</td><td>${formatNumber(active_cases)}</td></tr>`
-      : "";
-    const tR = total_recovered
-      ? `<tr><td>🚶 Recovered</td><td>${formatNumber(
-          total_recovered
-        )}</td></tr>`
-      : "";
-    const nC = new_cases
-      ? `<tr><td>⚡ New Cases</td><td>${formatNumber(new_cases)}</td></tr>`
-      : "";
+    const { cases, deaths, total_recovered, serious_critical, active_cases, new_cases, country_name, flag } = object;
+    const dN = deaths ? `<tr><td>⚰️ Deaths</td><td>${formatNumber(deaths)}</td></tr>` : '';
+    const sC = serious_critical ? `<tr><td>🚑 Serious</td><td>${formatNumber(serious_critical)}</td></tr>` : '';
+    const aC = active_cases ? `<tr><td>🛌 Active</td><td>${formatNumber(active_cases)}</td></tr>` : '';
+    const tR = total_recovered ? `<tr><td>🚶 Recovered</td><td>${formatNumber(total_recovered)}</td></tr>` : '';
+    const nC = new_cases ? `<tr><td>⚡ New Cases</td><td>${formatNumber(new_cases)}</td></tr>` : '';
     el.innerHTML = `
         <h3><span style="font-size: 200%">${flag}</span><span>&nbsp; ${country_name.toUpperCase()}</span></h3>
         <table>${nC}</table>
@@ -45,41 +22,41 @@ export const showStats = ({ object, x, y }) => {
           <tr><td>🦠 Cases</td><td>${formatNumber(cases)}</td></tr>
         </table>
       `;
-    el.style.display = "block";
+    el.style.display = 'block';
     el.style.opacity = 0.7;
-    el.style.left = x + "px";
-    el.style.top = y + "px";
+    el.style.left = x + 'px';
+    el.style.top = y + 'px';
   } else {
     el.style.opacity = 0.0;
   }
 };
 
-export const scatterplot = source_data => {
+export const scatterplot = (source_data) => {
   return new ScatterplotLayer({
-    id: "scatter",
+    id: 'scatter',
     data: source_data,
     opacity: 0.7,
     filled: true,
     radiusMinPixels: 5,
     radiusMaxPixels: 100,
     radiusScale: 8,
-    getPosition: d => [d.longitude, d.latitude],
-    getFillColor: d => (d.deaths < 1 ? [255, 255, 0, 100] : [255, 0, 0, 100]),
-    getRadius: d => (d.active_cases < 50 ? 5 : d.active_cases + d.deaths * 10),
+    getPosition: (d) => [d.longitude, d.latitude],
+    getFillColor: (d) => (d.deaths < 1 ? [255, 255, 0, 100] : [255, 0, 0, 100]),
+    getRadius: (d) => (d.active_cases < 100 ? 5 : d.active_cases + d.deaths * 2),
     getLineWidth: 4,
-    getLineColor: "#000000",
+    getLineColor: '#000000',
     pickable: true,
     onHover: showStats,
-    onClick: showStats
+    onClick: showStats,
   });
 };
 
-export const heatmap = source_data => {
+export const heatmap = (source_data) => {
   return new HeatmapLayer({
-    id: "heat",
+    id: 'heat',
     data: source_data || [],
-    getPosition: d => [d.longitude, d.latitude],
-    getWeight: d => d.cases + d.deaths * 100 + d.new_cases * 10,
-    radiusPixels: 120
+    getPosition: (d) => [d.longitude, d.latitude],
+    getWeight: (d) => d.active_cases + d.new_cases * 2,
+    radiusPixels: 120,
   });
 };
